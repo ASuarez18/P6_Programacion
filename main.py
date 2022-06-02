2757a7ea002559ba4b41635f8768bc775a8e1e1a
Alejandro Suarez <suga.gonza.18@gmail.com>
2022-06-02 17:26:18 -0500

    Commit con programa finalizado

--- main.py
+++ main.py
@@ -1,22 +1,52 @@
-from multiprocessing import Pool as pl
+import math as mth
 import time as tm
+import multiprocessing as mp
 
-def convencional(n):
-    res = 0
+def primos(n):
+    arr = [True]*n
+    # res = 0
+    for i in range(n):
+        if i < 2:
+            arr[i] = False
+        elif i > 2:
+            for j in range(2, mth.ceil(mth.sqrt(i))+1):
+                if i % j == 0:
+                    arr[i] = False
+                    break
+        # if arr[i] == True:
+            # res += i
+    return arr
+
+def primos_multi(n):
     if n < 2:
-        return False
+        return n, False
+    elif n == 2:
+        return n, True
     else:
-        for i in range(2, n):
-            crec = 2
-            primo = True
-            while primo and crec < i:
-                if i % crec == 0:
-                    primo = False
-                else:
-                    crec += 1
-            if primo:
-                res += i
+        for j in range(2, mth.ceil(mth.sqrt(n)) +  1):
+            if n % j == 0:
+                return n, False
+    return n, True
 
-    return res
-t = tm.time()
-print("Suma:", convencional(500000), "\nTiempo:", tm.time() - t)
+if __name__ == "__main__":
+    # Single
+    n = 5000000
+    st = tm.time()
+    res = primos(n)
+    print(res[:30])
+    en = tm.time()
+    print("Tiempo single:", en-st)
+
+    # multi
+    st1 = tm.time()
+    n_arr = range(n)
+    n_process = 10
+    with mp.Pool(processes=n_process) as pool:
+        res = pool.map(primos_multi, n_arr)
+    pool.close()
+    print(res[:30])
+    en1 = tm.time()
+    print("Tiempo multi:", en1-st1)
+
+
+print(primos(10))
